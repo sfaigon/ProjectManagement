348431bcd185bc2e3c2d6ef24519648d0df488e8
csposito1220 <csposito1220@gmail.com>
2023-12-15 09:21:58 -0500

    Set up project model

---
+++ models/Project.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 6;
+
+const projectSchema = new Schema({
+  name: { type: String, required: true },
+  dateCreated: Date,
+  teamMembers: [],
+  tasks: [],
+  comments: {},
+});
