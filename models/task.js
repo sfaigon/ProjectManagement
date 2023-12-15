7e1e476dd6ca8549064f859076b2a7fe828c2e6a
Abi Samson <abi.samson97@yahoo.com>
2023-12-15 10:29:35 -0500

    Add route for creating task

--- models/task.js
+++ models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const taskSchema = new mongoose.Schema({
+const taskSchema = new Schema({
     taskId: {
       type: Number,
       required: true,
