c1cdcf74c5cca676f2d4ba557876ee18fdbf3953
Abi Samson <abi.samson97@yahoo.com>
2023-12-15 11:47:37 -0500

    Create Task form UI

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
+// default date for form fields
+const defaultDate = new Date(); 
+
 const TaskForm = ({ onSubmit }) => {
-  // State to manage form fields
+  // Manage form fields
   const [formData, setFormData] = useState({
     title: '',
-    deadline: '',
+    dateAssigned: defaultDate,
+    deadline: defaultDate,
     description: '',
+    stage: 'In Progress',
+
   });
 
   // Handle input changes
@@ -25,44 +31,61 @@ const TaskForm = ({ onSubmit }) => {
 
     // Reset form fields after submission 
     setFormData({
-      title: '',
-      deadline: '',
-      description: '',
+        title: '',
+        dateAssigned: defaultDate,
+        deadline: defaultDate,
+        description: '',
+        stage: 'In Progress',
     });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title:</label>
+      <label>Title:</label>
       <input
         type="text"
-        id="title"
         name="title"
         value={formData.title}
         onChange={handleInputChange}
         required
       />
 
-      <label htmlFor="deadline">Deadline:</label>
+     <label>Date Assigned:</label>
       <input
         type="date"
-        id="deadline"
+        name="dateAssigned"
+        value={formData.dateAssigned}
+        onChange={handleInputChange}
+        required
+      />
+
+      <label>Deadline:</label>
+      <input
+        type="date"
         name="deadline"
         value={formData.deadline}
         onChange={handleInputChange}
         required
       />
 
-      <label htmlFor="description">Description:</label>
+      <label>Description:</label>
       <textarea
-        id="description"
         name="description"
         value={formData.description}
         onChange={handleInputChange}
         required
       ></textarea>
-
-      {/* Add other form fields as needed */}
+    
+        <label>Stage:</label>
+        <select
+            name="stage"
+            value={formData.stage}
+            onChange={handleInputChange}
+        >
+        <option value="To Do">To Do</option>
+        <option value="In Progress">In Progress</option>
+        <option value="Done">Done</option>
+        </select>
 
       <button type="submit">Create Task</button>
     </form>
--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,5 +1,10 @@
+import TaskForm from "../../components/TaskForm/TaskForm";
+
 export default function TaskPage() {
     return (
+    <>
         <h1>My Tasks</h1>
+        < TaskForm /> 
+    </>
     )
 }
