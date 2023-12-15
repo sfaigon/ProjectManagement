060d1e354aeae7d9bd050fa6f2c294a9b239871c
chinobin <sfaigon@email.com>
2023-12-15 10:50:02 -0500

    create controller/api/comment.js

---
+++ controllers/api/comment.js
@@ -0,0 +1,25 @@
+const Comment = require("../../models/comment");
+
+module.exports = {
+  create,
+  index,
+};
+
+async function create(req, res) {
+  try {
+    const comment = await Comment.create(req.body);
+    const allComments = await Comment.find({ project: req.body.project });
+    res.json(allComments);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function index(req, res) {
+    try {
+        const allComments = await Comment.find({project: req.body.project});
+        res.json(allComments);
+    } catch (err) {
+        console.log(error);
+    }
+}
