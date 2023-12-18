6042d5ac7932e332ec625671de6976800532f623
Abi Samson <abi.samson97@yahoo.com>
2023-12-18 09:39:53 -0500

    Add index function for tasks

--- controllers/api/tasks.js
+++ controllers/api/tasks.js
@@ -2,7 +2,17 @@ const Task = require('../../models/task');
 
 module.exports = {
     create,
+    index
   };
+
+async function index(req,res) {
+    try {
+        const tasks = await Task.find({ userRecommending: req.user._id });
+        res.render('/tasks', { tasks });
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+}
+};
 
 async function create(req, res) {
     const task = await new Task(req.body);
