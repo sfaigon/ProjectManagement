701d44c13db07655475708462ae585bd6e7e4c15
chinobin <sfaigon@email.com>
2023-12-18 09:39:11 -0500

    comments  outline done

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const commentsCtrl = require("../../controllers/api/comments");
 
+
+// will change once projects is done
 router.get("/", commentsCtrl.index);
 router.post("/", commentsCtrl.create);
 
