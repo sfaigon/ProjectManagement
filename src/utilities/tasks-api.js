c20669bcc62d3d4d1856febb9b3f7ce078df12f5
Abi Samson <abi.samson97@yahoo.com>
2023-12-19 11:42:19 -0500

    add update task function in tasks-api

--- src/utilities/tasks-api.js
+++ src/utilities/tasks-api.js
@@ -1,6 +1,9 @@
 import sendRequest from './send-request';
 const BASE_URL = '/api/tasks';
 
+export async function updateTask(id, updates) {
+  return sendRequest(`${BASE_URL}/${id}`, 'PUT', updates);
+}
 export async function createTask(taskData) {
     return sendRequest(BASE_URL, "POST", taskData);
   }
@@ -12,3 +15,5 @@ export function getAll() {
 export function getById(id) {
     return sendRequest(`${BASE_URL}/${id}`, "GET");
   }
+
+
