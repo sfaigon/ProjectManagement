c1bc4e2575e6e0af16abe2f12a44eb794e37e783
Abi Samson <abi.samson97@yahoo.com>
2023-12-19 10:16:13 -0500

    create route for editing a task

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -5,5 +5,6 @@ const tasksCtrl = require("../../controllers/api/tasks");
 router.post("/", tasksCtrl.create);
 router.get("/", tasksCtrl.index);
 router.get("/:id", tasksCtrl.show);
+router.get("/:id/edit", tasksCtrl.edit);
 
 module.exports = router;
