f9c6d5b5cb59f6aa088cb1f43db7ee7b30218353
chinobin <sfaigon@email.com>
2023-12-19 15:42:58 -0500

    crud, controllers, and routes done

--- controllers/api/comments.js
+++ controllers/api/comments.js
@@ -3,7 +3,12 @@ const Comment = require("../../models/comment");
 module.exports = {
   create,
   index,
-  // show,
+  show,
+  edit,
+  update,
+  deleteComment,
+
+
 };
 
 async function create(req, res) {
@@ -23,5 +28,80 @@ async function index(req, res) {
     res.json(allComments);
   } catch (err) {
     console.log(err);
+  }
+}
+
+ 
+async function show(req, res) {
+  try {
+    const commentId = req.params.id;
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+  }
+
+  return res.json(comment);
+} catch (err) {
+  console.error(err);
+  return res.status(500).send({
+      message: 'Internal server error',
+  });
+  }
+}
+
+async function edit(req, res) {
+  try {
+    const commentId = req.params.index;
+    const updates = req.body;
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    Object.assign(comment, updates);
+
+    await comment.save();
+
+    res.json(comment);
+    } catch (err) {
+    console.error('Error editing comment:', err);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const commentId = req.params.id;
+    const updates = req.body;
+
+    const comment = await Comment.findByIdAndUpdate(commentId, updates, { new: true });
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    res.json(comment);
+  } catch (err) {
+    console.error('Error updating comment:', err);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+  
+async function deleteComment(req, res) {
+  try {
+    const commentId = req.params.id;
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+        return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    res.json({ message: 'Comment deleted successfully', deletedComment });
+  } catch (err) {
+    console.error('Error deleting comment:', err);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 }
