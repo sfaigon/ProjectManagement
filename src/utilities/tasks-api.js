62ce99f2211f6a0b32a9f5d6c2c25091626c3ac1
chinobin <sfaigon@email.com>
2023-12-20 10:54:21 -0500

    merge

--- src/utilities/tasks-api.js
+++ src/utilities/tasks-api.js
@@ -1,23 +1,21 @@
-import sendRequest from './send-request';
-const BASE_URL = '/api/tasks';
+import sendRequest from "./send-request";
+const BASE_URL = "/api/tasks";
 
 export async function deleteTask(id) {
-  return sendRequest(`${BASE_URL}/${id}`, 'DELETE');
+  return sendRequest(`${BASE_URL}/${id}`, "DELETE");
 }
 
 export async function updateTask(id, updates) {
-  return sendRequest(`${BASE_URL}/${id}`, 'PUT', updates);
+  return sendRequest(`${BASE_URL}/${id}`, "PUT", updates);
 }
 export async function createTask(taskData) {
-    return sendRequest(BASE_URL, "POST", taskData);
-  }
+  return sendRequest(BASE_URL, "POST", taskData);
+}
 
 export function getAll() {
   return sendRequest(BASE_URL);
 }
 
 export function getById(id) {
-    return sendRequest(`${BASE_URL}/${id}`, "GET");
-  }
-
-
+  return sendRequest(`${BASE_URL}/${id}`, "GET");
+}
