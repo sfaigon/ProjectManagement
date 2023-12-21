0cba8c44633cacce5974cad9d34c61ab028d5a03
chinobin <sfaigon@email.com>
2023-12-20 20:59:56 -0500

    picture

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,10 @@
+import projectIcon from "../../assests/projecticon.jpg"
 export default function WelcomePage() {
   return (
     <>
       <h1>Welcome to Project Manager</h1>
       <h2>~Create, Collab, Connect~</h2>
+      <img src={projectIcon} alt="icon" />
     </>
   );
 }
