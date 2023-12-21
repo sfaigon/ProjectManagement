cb1bcd7e4539f6b20fe5b1fc14b3c456a9fbee75
csposito1220 <csposito1220@gmail.com>
2023-12-20 20:21:39 -0500

    Get fixing

--- src/pages/ProjectShowPage/ProjectShowPage.jsx
+++ src/pages/ProjectShowPage/ProjectShowPage.jsx
@@ -7,7 +7,6 @@ import * as usersAPI from "../../utilities/users-api";
 import CommentForm from "../../components/CommentForm/CommentForm";
 import TaskIndex from "../../components/TaskIndex/TaskIndex";
 import "./ProjectShowPage.css";
-import Button from "@mui/material/Button";
 
 export default function ProjectShowPage({ user }) {
   const location = useLocation();
@@ -91,17 +90,19 @@ export default function ProjectShowPage({ user }) {
                 <p>Date Created: {formatDate(project.dateCreated)}</p>
               </div>
               <div>
-                <p>Team Members: {project.teamMembers}</p>
+                <p>Team Members: {teamMembers.join(", ")}</p>
               </div>
             </div>
-            <div className="crud-btns">
-              <Link to={`/projects/${projectId}/edit`}>
-                <button>Update</button>
-              </Link>
-              <button className="delete-btn" onClick={handleDelete}>
-                Delete Project
-              </button>
-            </div>
+            {project.user == user._id && (
+              <div className="crud-btns">
+                <Link to={`/projects/${projectId}/edit`}>
+                  <button>Update</button>
+                </Link>
+                <button className="delete-btn" onClick={handleDelete}>
+                  Delete Project
+                </button>
+              </div>
+            )}
             <div className="task-comment">
               <div className="task-container">
                 <TaskIndex project={project} />
@@ -149,62 +150,7 @@ export default function ProjectShowPage({ user }) {
             </div>
           </div>
         </div>
-        {/* <div> */}
-          {/* <h1>{project.name}</h1>
-        </div>
-        <div>
-          <p>Created By: {projectUser.name}</p>
-        </div>
-        <div>
-          <p>Date Created: {formatDate(project.dateCreated)}</p>
-        </div>
-        <div>
-          <p>Team Members: {teamMembers.join(", ")}</p>
-        </div>
-
-        {project.user == user._id && (
-          <>
-            <Link to={`/projects/${projectId}/edit`}>
-              <Button variant="contained">Update</Button>
-            </Link>
-            <Button
-              className="delete-btn"
-              variant="contained"
-              onClick={handleDelete}
-            >
-              Delete Project
-            </Button>
-            <br />
-            <Link
-              to={{
-                pathname: `/projects/${projectId}/tasks/create`,
-                state: { projectId: project._id },
-              }}
-            >
-              <button>Create Task</button>
-            </Link>
-          </>
-        )}
-
-        <TaskIndex project={project} />
-
-        <CommentForm user={user} projectId={project} onSubmit={addComment} />
-        <ul>
-          {comments.map(
-            (c, idx) =>
-              project._id == c.project &&
-              (user._id == c.user ? (
-                <li key={idx}>
-                  <Link to={`/comments/${c._id}`}>
-                    <p>{c.text}</p>
-                  </Link>
-                </li>
-              ) : (
-                <p key={idx}>{c.text}</p>
-              ))
-          )}
-        </ul>
       </div>
     </>
   );
-} */}
+}
