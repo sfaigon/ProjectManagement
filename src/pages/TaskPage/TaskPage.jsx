57bbfc4e15ba1d1ff7e990841477be2a42edf814
csposito1220 <csposito1220@gmail.com>
2023-12-20 19:19:24 -0500

    Finish user and task page

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -2,36 +2,43 @@ import { useState, useEffect } from "react";
 import * as tasksAPI from "../../utilities/tasks-api";
 import TaskForm from "../../components/TaskForm/TaskForm";
 import { Link } from "react-router-dom";
+import "./TaskPage.css";
 
 export default function TaskPage() {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-    function addTask(newTask) {
-        setTasks([...tasks, newTask]);
+  function addTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  useEffect(function () {
+    async function getTasks() {
+      const tasks = await tasksAPI.getAll();
+      setTasks(tasks);
     }
+    getTasks();
+  }, []);
 
-    useEffect(function() {
-        async function getTasks() {
-            const tasks = await tasksAPI.getAll();
-            setTasks(tasks);
-        }
-        getTasks();
-    }, []);  
-    
-    
-
-    return (
+  return (
     <>
-        <h1>My Tasks</h1>
-        <ul>
-            {tasks.map((task, index) => (
-                <li key={index}>
-                    <Link to={`/tasks/${task._id}`}>
+      <div className="container">
+        <div className="tasklist-container">
+          <div className="title">
+            <h1>My Tasks</h1>
+          </div>
+          <div className="task-page-list">
+            <ul>
+              {tasks.map((task, index) => (
+                <li key={index} className="task">
+                  <Link to={`/tasks/${task._id}`}>
                     <strong>{task.title}</strong>
-                    </Link>
+                  </Link>
                 </li>
-            ))}
-        </ul>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
